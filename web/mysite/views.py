44dd4c9bd2564827ccd23ba26480671f611a0e14
Dominik Adam <adam@inqool.cz>
2024-04-26 01:28:35 +0200

    add evaluator

--- web/mysite/views.py
+++ web/mysite/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
+from web.algorithms.evaluator import Evaluator
 from web.algorithms.naive_rec import load_data, get_most_popular
+from web.algorithms.random_recommender import random_rating_recommender
 
 
 def index(request, category=None, page=1):
@@ -28,3 +30,14 @@ def article_detail(request, article_id):
     article_df = articles_df[articles_df['ID'] == article_id]
     article = article_df.to_dict('records')[0]
     return render(request, 'detail.html', {'article': article, 'recommended_articles': recommended_articles})
+
+def evaluation(request):
+    evaluator = Evaluator()
+    recommenders = {
+        "Random recommender": random_rating_recommender
+    }
+    evals = [{
+        "name": name,
+        "evaluation": [round(result, 4) for result in evaluator.evaluate(recommender)]
+    } for name, recommender in recommenders.items()]
+    return render(request, 'evaluation.html', {'recommenders': evals})
